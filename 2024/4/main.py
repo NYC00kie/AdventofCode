d0b17550a61bafacf83aa93d609471b4642ca675
Nici <nici.scheel@gmail.com>
2024-12-13 15:22:15 +0100

    works now, I counted wrong with indicies

--- 2024/4/main.py
+++ 2024/4/main.py
@@ -1,3 +1,6 @@
+#2500 too low
+#2550 too high
+
 import numpy as np
 file1 = open("my_file.csv", "r").read()
 count = 0
@@ -19,14 +22,16 @@ for i in range(len(file1)-3):
 			count += 1
 			print(i,j)
 print(count)
-for i in range(3,len(file1)):
+for i in range(len(file1)-3):
 	for j in range(3,len(file1[i])):
-		if file1[i][j] == "X" and file1[i-1][j-1] == "M" and file1[i-2][j-2] == "A" and file1[i-3][j-3] == "S":
+		print(1,count)
+		if file1[i][j] == "X" and file1[i+1][j-1] == "M" and file1[i+2][j-2] == "A" and file1[i+3][j-3] == "S":
 			count += 1
 			print(i,j)
-		elif file1[i][j] == "S" and file1[i-1][j-1] == "A" and file1[i-2][j-2] == "M" and file1[i-3][j-3] == "X":
+		elif file1[i][j] == "S" and file1[i+1][j-1] == "A" and file1[i+2][j-2] == "M" and file1[i+3][j-3] == "X":
 			count += 1
 			print(i,j)
+		print(2,count)
 
 
 print(count)
